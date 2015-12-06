f373284363f9edc5cf1ad8da074a89ee91de6bf4
Masaichi Ueta <masamichi1023@gmail.com>
2015-12-06 22:23:38 +0900

    Modify resize function

--- js/site.js
+++ js/site.js
@@ -1,9 +1,15 @@
 $(function() {
   
-  console.log("Hello, I'm Masmaichi");
+  console.log('Hello, I\'m Masmaichi');
   
-  $(window).on("resize", function() {
-    console.log("resize");
+  $(window).on('resize', function() {
+
+    if ($('.sn-logo').css('display') === 'block') {
+      console.log('pc');
+    } else {
+      console.log('tablet, phone');
+    }
+    
   });
   
 });
