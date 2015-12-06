6b8428bb9517abba1fe94e843fff267d6164e641
Masaichi Ueta <masamichi1023@gmail.com>
2015-12-06 22:43:49 +0900

    Modify resize function

--- js/site.js
+++ js/site.js
@@ -1,17 +1,28 @@
 $(function() {
   
   $(window).on('resize', function() {
-
+    
+    //Show PC navigation for PC
     if ($('.sn-logo').css('display') === 'block') {
       
       var menustate = $('#dn-menustate');
- 
+      
+      //CheckBox
       if (menustate.prop('checked')) {
+        
+        //Add notransition class temporary
+        $('#nav').addClass('notransition');
         menustate.prop('checked', false);
+        $('#nav').removeClass('notransition');
       } 
       
+      //Anchor
       if (window.location.hash === '#dn-menustate') {
+        
+        //Add notransition class temporary
+        $('#nav').addClass('notransition');
         window.location.hash = '#';
+        $('#nav').removeClass('notransition');
       }
     }
     
