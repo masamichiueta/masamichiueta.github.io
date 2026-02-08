5001fb91709f14b5a8b1e6db5392003f4d1c1cd1
Masamichi Ueta <u_masamichi@icloud.com>
2026-02-08 20:28:34 +0900

    build

--- js/site.js
+++ js/site.js
@@ -1,39 +1,3 @@
-$(function() {
-  
-  $(window).on('resize', function() {
-    
-    //Show PC navigation for PC
-    if ($('.sn-logo').css('display') === 'block') {
-      
-      var menustate = $('#dn-menustate');
-      var nav = $('#nav');
-      
-      //CheckBox
-      if (menustate.prop('checked')) {
-        
-        //Add notransition class temporary
-        nav.addClass('notransition');
-        menustate.prop('checked', false);
-        nav[0].offsetHeight;
-        nav.removeClass('notransition');
-      } 
-      
-      //Anchor
-      if (window.location.hash === '#dn-menustate') {
-        
-        //Add notransition class temporary
-        nav.addClass('notransition');
-        window.location.hash = '#';
-        nav[0].offsetHeight;
-        nav.removeClass('notransition');
-      }
-    }
-    
-  });
-  
-});
-
-
 /*!
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
  * Copyright 2011-2023 The Bootstrap Authors
